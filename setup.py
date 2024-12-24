97a937eb4ff5864aafa046a24ee970919eafe585
kuankuan2007 <2163826131@qq.com>
2024-12-25 04:13:11 +0800

    change version to 0.1.1 & range requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="autoDownload",
-    version="0.1.0",
+    version="0.1.1",
     keywords=["requests", "download", "http", "thread"],
     description="A simple, efficient, general-purpose Python multithreaded download library",
     long_description=open("README.md", "r", encoding="utf-8").read(),
